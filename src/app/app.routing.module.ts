251c7ee8a3c117d4438b3b934200ada75acf9273
Sebas15897 <sebastiancontreras15897@gmail.com>
2023-09-06 23:48:34 -0500

    feat: Routes are implemented for a private and a public module

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/public/public.module').then((m) => m.PublicModule),
+  },
+  {
+    path: 'private',
+    loadChildren: () =>
+      import('./pages/private/private.module').then((m) => m.PrivateModule),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
