4eed64083bb90507538097f5d41fdd69cc56c3dc
Sebas15897 <sebastiancontreras15897@gmail.com>
2023-09-07 01:09:12 -0500

    feat: The login component is terminated

--- src/app/pages/public/login/login.module.ts
+++ src/app/pages/public/login/login.module.ts
@@ -2,9 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login.routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [CommonModule, LoginRoutingModule],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+  ],
   declarations: [LoginComponent],
 })
 export class LoginModule {}
